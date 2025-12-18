fb6d180137a20ca126e3cc59d26a75f68afb3767
Aleksandr5667 <xtrack89@gmail.com>
2025-12-18 00:33:58 +0000

    Ок

--- screens/settings/SettingsScreen.tsx
+++ screens/settings/SettingsScreen.tsx
@@ -222,7 +222,7 @@ export default function SettingsScreen({ navigation }: Props) {
         <SettingsItem
           icon="code"
           label={t("settings.version")}
-          value="1.2.4"
+          value="1.2.5"
         />
         <SettingsItem
           icon="shield"
